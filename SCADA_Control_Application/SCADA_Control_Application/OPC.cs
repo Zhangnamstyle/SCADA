e3470596ec572e5300b396e43fa9b5774c33f868
Alexander Zhang Gjerseth <alexandergjerseth@gmail.com>
2016-04-21 01:36:55 +0200

    Started to implement OPC class

---
+++ SCADA_Control_Application/SCADA_Control_Application/OPC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SCADA_Control_Application
+{
+    public class OPC
+    {
+        private string opcConURL;
+
+        public OPC(string opcURL)
+        {
+            this.opcConURL = opcURL;
+        }
+
+        public void writeToOPC(double opcValue)
+        {
+            
+        }
+    }
+}
