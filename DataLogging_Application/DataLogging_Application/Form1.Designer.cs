99db093d6ebb8007e5a325baa4717f113bab952c
Kim Aars <kim.aars1@gmail.com>
2016-04-21 10:47:03 +0200

    Added OPC class

---
+++ DataLogging_Application/DataLogging_Application/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DataLogging_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtBxOpcVal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(34, 51);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBxOpcVal
+            // 
+            this.txtBxOpcVal.Location = new System.Drawing.Point(202, 53);
+            this.txtBxOpcVal.Name = "txtBxOpcVal";
+            this.txtBxOpcVal.Size = new System.Drawing.Size(100, 20);
+            this.txtBxOpcVal.TabIndex = 1;
+            this.txtBxOpcVal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 261);
+            this.Controls.Add(this.txtBxOpcVal);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtBxOpcVal;
+    }
+}
+
